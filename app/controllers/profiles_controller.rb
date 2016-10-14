92553f1e000150360fd6921f4017ded9f562d5fd
Marvin Messenzehl <marvin.messenzehl@me.com>
2016-10-14 07:53:34 +0000

    Added before filters to check current user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user! #Devise Action for authorization
+    before_action :only_current_user
+    
     def new
         # form where a user can fill out their own profile
         @user = User.find(params[:user_id]) #Find user from db which is logged in
@@ -21,11 +24,27 @@ class ProfilesController < ApplicationController
         @profile = @user.profile
     end
     
+    def update
+        @user = User.find (params[:user_id])
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile Updated!"
+            redirect_to user_path(params[:user_id])
+        else
+            render action: :edit
+        end
+    end
+    
     
     
     private
     def profile_params
         #whitelist params for database
         params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+        @user = User.find (params[:user_id])
+        redirect_to(root_url) unless @user == current_user #current user from devise
     end
 end
